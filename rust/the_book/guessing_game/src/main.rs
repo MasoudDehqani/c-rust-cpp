853b3f49a716c3039e11be441304419f993630be
Masoud <masoud67.md@gmail.com>
2025-08-20 07:52:28 +0330

    rust: re-write guessing game code

--- rust/the_book/guessing_game/src/main.rs
+++ rust/the_book/guessing_game/src/main.rs
@@ -1,34 +1,37 @@
-use colored::*;
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io::stdin;
+use colored::{Color, Colorize};
+use rand::random_range;
+use std::{cmp::Ordering, io::stdin};
 
 fn main() {
-    let secret: u8 = rand::rng().random_range(..101);
-    println!("The secret is {secret}");
+    println!("Enter your guess: ");
+    let secret_number: u8 = random_range(0..=100);
 
     loop {
-        let mut guess = String::new();
-        println!("Enter the number you guessed:");
-        stdin()
-            .read_line(&mut guess)
-            .expect("input cannot be read!");
+        let mut user_input_guess = String::new();
 
-        let guess: u8 = guess.trim().parse().expect("Cannot be parsed");
-        match guess.cmp(&secret) {
-            Ordering::Less => {
-                println!("{}", "TOO SMALL".yellow());
+        stdin()
+            .read_line(&mut user_input_guess)
+            .expect("cannot read standard input");
+
+        let processed_user_guess: u8 = match user_input_guess.trim().parse() {
+            Ok(guess) if guess <= 100 => guess,
+            _ => {
+                println!("Enter a number between 0 and 100");
                 continue;
+            }
+        };
+
+        match processed_user_guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("{}", "You WON!".color(Color::BrightGreen));
+                break;
             }
             Ordering::Greater => {
-                println!("{}", "TOO BIG".red());
-                continue;
+                println!("{}", "TOO BIG!".color(Color::Red));
             }
-            Ordering::Equal => println!("{}", "You WON".green()),
-        }
-
-        if guess == secret {
-            break;
+            Ordering::Less => {
+                println!("{}", "TOO SMALL!".color(Color::BrightYellow));
+            }
         }
     }
 }
