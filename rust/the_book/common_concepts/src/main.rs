977517bf2f533b9e1b4aa5dda39c30337554a503
Masoud <masoud67.md@gmail.com>
2025-08-20 14:31:34 +0330

    rust: chapter 3, common_programming_concept, added

---
+++ rust/the_book/common_concepts/src/main.rs
@@ -0,0 +1,9 @@
+/*
+    - values (variable) which are bind to a name or identifier are
+    by default immutable in Rust
+    - immutability contributes to safety and easy concurrency
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
