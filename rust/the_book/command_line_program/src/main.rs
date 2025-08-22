7d555a742c395204eaccf3aaaf5741506d2583cb
Masoud <masoud67.md@gmail.com>
2025-08-22 06:03:15 +0330

    rust: a trivial code added for command_line_program

--- rust/the_book/command_line_program/src/main.rs
+++ rust/the_book/command_line_program/src/main.rs
@@ -5,8 +5,28 @@
 */
 
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("searching for {query} in the file {file_path}");
+
+    let mut file_content = String::new();
+    let file = File::open(file_path);
+
+    file.iter().for_each(|mut f| {
+        let r = f.read_to_string(&mut file_content);
+        r.expect("");
+    });
+
+    let found = file_content.find(query);
+    match found {
+        Some(i) => println!("{}", file_content.split_off(i)),
+        None => println!("Not Found"),
+    };
 }
