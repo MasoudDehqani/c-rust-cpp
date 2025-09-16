65ac8278bfba1ac75de9143957ed8ed7d5229e43
Masoud <masoud67.md@gmail.com>
2025-09-16 07:03:51 +0330

    Rust: the project for lox interpreter added

---
+++ rust/projects/lox_rs/src/main.rs
@@ -0,0 +1,20 @@
+/*
+  - Parsing has a long, rich history in computer science that is closely tied to the
+  artificial intelligence community. Many of the techniques used today to parse programming
+  languages were originally conceived to parse human languages by AI researchers who were
+  trying to get computers to talk to us
+
+  - scanning - lexing - lexical analysis - scanner - lexer - token
+  - parsing - grammar - parser - parse tree - abstract syntax tree (AST) - syntax tree - syntax error
+  - static analysis - binding - resolution - identifier - scope - type error - attributes -
+  symbol table
+  - everything up to this point is considered the font end of the implementation
+  - middle end and back end remains
+  - intermediate representation (IR)
+  - optimization - constant folding
+  - code generation -> back end
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
