4c618049076325e120cd1ff39693fafc8f783361
Masoud <masoud67.md@gmail.com>
2025-12-14 08:55:36 +0330

    Rust: more code examples of async chapter of the book added

--- rust/the_book/asynchronous_programming/src/lib.rs
+++ rust/the_book/asynchronous_programming/src/lib.rs
@@ -5,11 +5,48 @@
   | `unbounded_channel` | `channel`         |
 */
 
-pub async fn get_page_title(url: &str) -> Option<String> {
+use std::pin::pin;
+
+use futures::future::{self, Either};
+
+// pub async fn get_page_title(url: &str) -> Option<String> {
+//     let res = reqwest::get(url).await.unwrap();
+//     let response_text = res.text().await.unwrap();
+//     scraper::Html::parse_document(&response_text)
+//         .select(&scraper::Selector::parse("title").unwrap())
+//         .nth(0)
+//         .map(|el| el.inner_html())
+// }
+
+pub async fn get_page_title(url: &str) -> (&str, Option<String>) {
     let res = reqwest::get(url).await.unwrap();
     let response_text = res.text().await.unwrap();
-    scraper::Html::parse_document(&response_text)
+    let maybe_title_text = scraper::Html::parse_document(&response_text)
         .select(&scraper::Selector::parse("title").unwrap())
         .nth(0)
-        .map(|el| el.inner_html())
+        .map(|el| el.inner_html());
+
+    (url, maybe_title_text)
+}
+
+pub async fn select<F1, F2, A, B>(f1: F1, f2: F2) -> Either<A, B>
+where
+    F1: Future<Output = A>,
+    F2: Future<Output = B>,
+{
+    let f1 = pin!(f1);
+    let f2 = pin!(f2);
+
+    match future::select(f1, f2).await {
+        Either::Left((a, _)) => Either::Left(a),
+        Either::Right((b, _)) => Either::Right(b),
+    }
+}
+
+pub async fn race<F1, F2, A, B>(f1: F1, f2: F2) -> Either<A, B>
+where
+    F1: Future<Output = A>,
+    F2: Future<Output = B>,
+{
+    select(f1, f2).await
 }
--- rust/the_book/asynchronous_programming/src/main.rs
+++ rust/the_book/asynchronous_programming/src/main.rs
@@ -1,15 +1,40 @@
-use asynchronous_programming::get_page_title;
+use asynchronous_programming::{get_page_title, race};
+use futures::future::Either;
 #[allow(unused_imports)]
 use tokio::runtime::Runtime;
 
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     let runtime = Runtime::new().unwrap();
+
     runtime.block_on(async {
-        let url = &args[1];
-        match get_page_title(url).await {
-            Some(title) => println!("Title is: {}", title),
-            None => println!("No Title"),
+        let url1 = &args[1];
+        let url2 = &args[2];
+
+        let fut_1 = get_page_title(url1);
+        let fut_2 = get_page_title(url2);
+
+        let (url, maybe_title) = match race(fut_1, fut_2).await {
+            Either::Left(l) => l,
+            Either::Right(r) => r,
+        };
+
+        println!("url {} won!", url);
+        match maybe_title {
+            Some(title) => println!("title is {}", title),
+            None => println!("No title!"),
         }
-    });
+    })
 }
+
+// fn main() {
+//     let args = std::env::args().collect::<Vec<String>>();
+//     let runtime = Runtime::new().unwrap();
+//     runtime.block_on(async {
+//         let url = &args[1];
+//         match get_page_title(url).await {
+//             Some(title) => println!("Title is: {}", title),
+//             None => println!("No Title"),
+//         }
+//     });
+// }
