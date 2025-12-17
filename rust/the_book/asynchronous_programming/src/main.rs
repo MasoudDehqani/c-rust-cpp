a281900682a57619545ca42b890101233caf8a87
Masoud <masoud67.md@gmail.com>
2025-12-17 06:27:16 +0330

    Rust: done with 17.3, third part of async chapter of the book

--- rust/the_book/asynchronous_programming/src/main.rs
+++ rust/the_book/asynchronous_programming/src/main.rs
@@ -3,6 +3,7 @@
 use std::{
     pin::{Pin, pin},
     process::Output,
+    thread,
 };
 
 use asynchronous_programming::{get_page_title, race};
@@ -13,49 +14,186 @@ use futures::{
 use tokio::{
     runtime::Runtime,
     sync::mpsc::unbounded_channel,
-    task::spawn,
-    time::{Duration, sleep},
+    task::{spawn, yield_now},
+    time::{Duration, Instant, sleep},
 };
 
-fn main() {
-    let runtime = Runtime::new().unwrap();
+#[tokio::main]
+async fn main() {
+    let slow = async {
+        sleep(Duration::from_secs(5)).await;
+        "slow finished"
+    };
 
-    runtime.block_on(async {
-        let (tx, mut rx) = unbounded_channel::<String>();
-        let tx2 = tx.clone();
-
-        let fut1 = async move {
-            let messages = vec!["hi", "from", "first", "future"];
-            for msg in messages {
-                tx.send(format!("message: {msg}")).unwrap();
-                sleep(Duration::from_millis(500)).await;
-            }
-        };
-
-        let fut2 = async move {
-            let messages = vec!["hello", "from2", "second", "future2"];
-            for msg in messages {
-                tx2.send(format!("{msg}")).unwrap();
-                sleep(Duration::from_millis(500)).await;
-            }
-        };
-
-        let fut3 = async {
-            while let Some(msg) = rx.recv().await {
-                println!("{}", msg);
-            }
-        };
-
-        // join3(fut1, fut2, fut3).await;
-        // join!(fut1, fut2, fut3);
-        let fut1 = Box::pin(fut1);
-        let fut2 = Box::pin(fut2);
-        let fut3 = Box::pin(fut3);
-        let futures: Vec<Pin<Box<dyn Future<Output = ()>>>> = vec![fut1, fut2, fut3];
-
-        join_all(futures).await;
-    })
+    match timeout(slow, Duration::from_secs(2)).await {
+        Ok(res) => println!("done with {res}"),
+        Err(s) => println!("failed to be done in {} secs", s.as_secs_f32()),
+    }
 }
+
+async fn timeout<F: Future>(fut: F, max_time: Duration) -> Result<F::Output, Duration> {
+    let res = race(fut, sleep(max_time)).await;
+
+    match res {
+        Either::Left(a) => Ok(a),
+        Either::Right(_) => Err(max_time),
+    }
+}
+
+/*
+  This is a form of cooperative multitasking.
+  In some Rust-based embedded operation systems, this is the only
+  kind of multitasking!
+*/
+// #[tokio::main]
+// async fn main() {
+//     let one_nanosecond = Duration::from_nanos(1);
+//     let start = Instant::now();
+//     async {
+//         for _ in 1..1000 {
+//             sleep(one_nanosecond).await;
+//         }
+//     }
+//     .await;
+//     let time = Instant::now() - start;
+//     println!("sleep version finished in {}", time.as_secs_f32());
+
+//     let start = Instant::now();
+//     async {
+//         for _ in 1..1000 {
+//             yield_now().await;
+//         }
+//     }
+//     .await;
+//     let time = Instant::now() - start;
+//     println!("yield version finished in {}", time.as_secs_f32());
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let a = async {
+//         println!("a started");
+//         slow("a", 30);
+//         yield_now().await;
+//         slow("a", 10);
+//         yield_now().await;
+//         slow("a", 20);
+//         yield_now().await;
+//         println!("a finished");
+//     };
+
+//     let b = async {
+//         println!("b started");
+//         slow("b", 75);
+//         yield_now().await;
+//         slow("b", 10);
+//         yield_now().await;
+//         slow("b", 15);
+//         yield_now().await;
+//         slow("b", 350);
+//         yield_now().await;
+//         println!("b finished");
+//     };
+
+//     race(a, b).await;
+// }
+
+// fn slow(name: &str, ms: u64) {
+//     thread::sleep(Duration::from_millis(ms));
+//     println!("{name} ran for {ms}ms");
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let a = async {
+//         println!("a started");
+//         slow("a", 30);
+//         sleep(Duration::from_millis(1)).await;
+//         slow("a", 10);
+//         sleep(Duration::from_millis(1)).await;
+//         slow("a", 20);
+//         sleep(Duration::from_millis(1)).await;
+//         println!("a finished");
+//     };
+
+//     let b = async {
+//         println!("b started");
+//         slow("b", 75);
+//         sleep(Duration::from_millis(1)).await;
+//         slow("b", 10);
+//         sleep(Duration::from_millis(1)).await;
+//         slow("b", 15);
+//         sleep(Duration::from_millis(1)).await;
+//         slow("b", 350);
+//         sleep(Duration::from_millis(1)).await;
+//         println!("b finished");
+//     };
+
+//     race(a, b).await;
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let slow = async {
+//         println!("slow started");
+//         sleep(Duration::from_millis(100)).await;
+//         println!("slow finished");
+//     };
+
+//     let fast = async {
+//         println!("fast started");
+//         sleep(Duration::from_millis(50)).await;
+//         println!("fast finished");
+//     };
+
+//     race(slow, fast).await;
+// }
+
+// fn main() {
+//     let runtime = Runtime::new().unwrap();
+
+//     runtime.block_on(async {
+//         let (tx, mut rx) = unbounded_channel::<String>();
+//         let tx2 = tx.clone();
+
+//         let fut1 = async move {
+//             let messages = vec!["hi", "from", "first", "future"];
+//             for msg in messages {
+//                 tx.send(format!("message: {msg}")).unwrap();
+//                 sleep(Duration::from_millis(500)).await;
+//             }
+//         };
+
+//         let fut2 = async move {
+//             let messages = vec!["hello", "from2", "second", "future2"];
+//             for msg in messages {
+//                 tx2.send(format!("{msg}")).unwrap();
+//                 sleep(Duration::from_millis(500)).await;
+//             }
+//         };
+
+//         let fut3 = async {
+//             while let Some(msg) = rx.recv().await {
+//                 println!("{}", msg);
+//             }
+//         };
+
+//         // join3(fut1, fut2, fut3).await;
+//         // join!(fut1, fut2, fut3);
+
+//         // let fut1 = Box::pin(fut1);
+//         // let fut2 = Box::pin(fut2);
+//         // let fut3 = Box::pin(fut3);
+//         // let futures: Vec<Pin<Box<dyn Future<Output = ()>>>> = vec![fut1, fut2, fut3];
+
+//         let fut1 = pin!(fut1);
+//         let fut2 = pin!(fut2);
+//         let fut3 = pin!(fut3);
+//         let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![fut1, fut2, fut3];
+
+//         join_all(futures).await;
+//     })
+// }
 
 // fn main() {
 //     let runtime = Runtime::new().unwrap();
