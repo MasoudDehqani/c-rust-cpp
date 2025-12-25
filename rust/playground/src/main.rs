746e0c66f7da941c92c4d0a49dfad6e848324e8b
Masoud <masoud67.md@gmail.com>
2025-12-25 07:24:31 +0330

    Rust: some example code added to playground

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,17 +1,67 @@
 fn main() {
-    let s = String::from("some string");
-    let s3 = second(s);
-    println!("{s3}");
+    let opt = Some(String::from("hello"));
+    let opt_ref = opt.as_ref();
 
-    let mut n = Box::new(1);
-    println!("{n}");
-    n = Box::new(2);
-    *n = 3;
-    println!("{n}")
+    let len = match opt_ref {
+        Some(s) => s.len(),
+        None => 0,
+    };
+
+    println!("{:?}", opt);
+    println!("{}", len)
 }
 
-fn second(s: String) -> String {
-    let mut s2 = String::from("second string ");
-    s2.push_str(&s);
-    s2
-}
+// struct Rust {
+//     name: String,
+//     ver: f64,
+// }
+
+// impl Rust {
+//     fn print_name(&self) {
+//         println!("{}", self.name)
+//     }
+
+//     fn print_version(&self) {
+//         println!("{}", self.ver)
+//     }
+// }
+
+// fn main() {
+//     let rust = Rust {
+//         name: String::from("rust"),
+//         ver: 1.19,
+//     };
+
+//     rust.print_name();
+//     rust.print_version();
+// }
+
+// fn main() {
+//     let rust_string = String::from("rust");
+
+//     print_rust(rust_string);
+
+//     println!("{rust_string}");
+// }
+
+// fn print_rust(s: String) {
+//     println!("{}", s + " prigramming language");
+// }
+
+// fn main() {
+//     let s = String::from("some string");
+//     let s3 = second(s);
+//     println!("{s3}");
+
+//     let mut n = Box::new(1);
+//     println!("{n}");
+//     n = Box::new(2);
+//     *n = 3;
+//     println!("{n}")
+// }
+
+// fn second(s: String) -> String {
+//     let mut s2 = String::from("second string ");
+//     s2.push_str(&s);
+//     s2
+// }
